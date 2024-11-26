3292e3fb69470e17654452a9e21c4adcdd4ab1d0
Xomnhala123 <134589369+Xomnhala123@users.noreply.github.com>
2024-11-26 10:53:37 +0700

    mới sửa

--- thoikhoabieuSV.php
+++ thoikhoabieuSV.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $sname = "localhost";
 $uname = "root";
 $password = "";
@@ -10,40 +12,49 @@ if (!$conn) {
     echo "Kết nối thất bại!";
 }
 
-$sql = "SELECT 
-            cahoc.ngayHoc AS thu,
-            cahoc.caHoc AS ca,
-            phancong.maPhong AS phong,
-            giangvien.hoTen AS tenGV
-        FROM 
-            phancong
-        JOIN 
-            cahoc 
-        ON 
-            phancong.maCH = cahoc.maCH
-        JOIN 
-            giangvien 
-        ON 
-            phancong.maGV = giangvien.maGV
-        ORDER BY 
-            cahoc.caHoc, cahoc.ngayHoc";
+    $sql = "SELECT 
+                cahoc.ngayHoc AS thu,
+                cahoc.caHoc AS ca,
+                phancong.maPhong AS phong,
+                giangvien.hoTen AS tenGV,
+                hocphan.tenHP AS tenHP
+            FROM 
+                phancong
+            JOIN 
+                cahoc 
+            ON 
+                phancong.maCH = cahoc.maCH
+            JOIN 
+                giangvien 
+            ON 
+                phancong.maGV = giangvien.maGV
+            JOIN
+                hocphan
+            ON
+                phancong.maLHP = hocphan.maLHP
+            JOIN
+                sinhvien
+            ON
+                sinhvien.khoa = hocphan.khoaQL
+            ORDER BY 
+                cahoc.caHoc, cahoc.ngayHoc";
 
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$result = $stmt->get_result();
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-$thoiKhoaBieu = [];
-while ($row = $result->fetch_assoc()) {
-    $thu = $row['thu'];
-    $ca = $row['ca'];
-    $phong = $row['phong'];
-    $tenGV = $row['tenGV'];
+    $thoiKhoaBieu = [];
+    while ($row = $result->fetch_assoc()) {
+        $thu = $row['thu'];
+        $ca = $row['ca'];
+        $phong = $row['phong'];
+        $tenGV = $row['tenGV'];
+        $tenHP = $row['tenHP'];
+        $thoiKhoaBieu[$ca][$thu] = "Môn:$tenHP-Phòng:$phong-GV:$tenGV";
+    }
 
-    $thoiKhoaBieu[$ca][$thu] = "Phòng: $phong GV: $tenGV";
-}
-
-$danhSachThu = ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'];
-$danhSachCa = [1, 2, 3, 4];
+    $danhSachThu = ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'];
+    $danhSachCa = [1, 2, 3, 4];
 ?>
 
 <!DOCTYPE html>
@@ -94,11 +105,16 @@ $danhSachCa = [1, 2, 3, 4];
                     <td><?php echo "Ca $ca"; ?></td>
                     <?php foreach ($danhSachThu as $thu): ?>
                         <td>
+                            <div>
                             <?php
-                            echo isset($thoiKhoaBieu[$ca][$thu]) 
-                                ? htmlspecialchars($thoiKhoaBieu[$ca][$thu]) 
-                                : "Trống";
+                            if(isset($thoiKhoaBieu[$ca][$thu]) ){
+                                $string = htmlspecialchars($thoiKhoaBieu[$ca][$thu]);
+                                $tt = explode("-",$string);
+                                echo $tt[0]."</div><div>".$tt[1]."</div><div>".$tt[2];
+                            }
+                            else echo "Trống";
                             ?>
+                            </div>
                         </td>
                     <?php endforeach; ?>
                 </tr>
